de574f937f88d8c34be1a4ac13bf2b544881ee59
npotternyc <nat.potter@gmail.com>
2024-08-04 22:44:17 -0400

    Fix persistance issues

--- genmon/run.sh
+++ genmon/run.sh
@@ -2,8 +2,13 @@ if [ ! -f "/var/log/genmon.log" ]; then
   touch /var/log/genmon.log
 fi
 
-if [ ! -d "/etc/genmon" ]; then
-  mkdir -p /etc/genmon
+if [ -d "/data/genmon" ]; then
+  echo "Link etc/genmon to /data/genmon"
+  sudo rm -r /etc/genmon 
+else
+  echo "Move and link etc/genmon to /data/genmon"
+  mv /etc/genmon /data
 fi
+sudo ln -s /data/genmon /etc/
 
 /app/genmon/startgenmon.sh start && tail -F /var/log/genmon.log
